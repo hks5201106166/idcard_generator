e89540656d9b8e509f80c7bed9542d9d566ab9b5
linux <729681512@qq.com>
2020-06-24 13:36:20 +0800

    t

--- add_idcard_on_blank.py
+++ add_idcard_on_blank.py
@@ -1,6 +1,7 @@
 #-*-coding:utf-8-*-
 import cv2
 import numpy as np
+import os
 def rotate_bound(image, angle):
     """
 
@@ -32,85 +33,100 @@ def rotate_bound(image, angle):
     return img
 tt=['background','up_obverse','up_reverse','drown_obverse','drown_reverse']
 image=cv2.imread('/home/simple/mydemo/ocr_project/idcard_generator_project/background_正反面_.jpg')
-front=cv2.imread('/home/simple/mydemo/ocr_project/idcard_generator_project/idcard_generator/0a7caef3c8324cfc902175b267009295_1.jpg')
-back=cv2.imread('/home/simple/mydemo/ocr_project/idcard_generator_project/idcard_generator/0a13a2e882514ddf87790beb337c9bf6_0.jpg')
+files=os.listdir('/home/simple/mydemo/ocr_project/idcard_generator_project/gen_data_with_logo')
+
+for file in files:
+    flag_front_back=file.split('_')[-2]
+    if flag_front_back=='1':
+        index=np.random.randint(0,4)
+        back_img_name=file.split('_')[0]+'_'+'0'+'_'+str(index)+'.jpg'
+
+        front=cv2.imread('/home/simple/mydemo/ocr_project/idcard_generator_project/gen_data_with_logo/'+file)
+        h,w,c=front.shape
+        front=front[:,0:int(w/2),:]
+        back=cv2.imread('/home/simple/mydemo/ocr_project/idcard_generator_project/gen_data_with_logo/'+back_img_name)
+        h_back,w_back,c_back=back.shape
+        back=back[:,0:int(w_back/2),:]
+        # cv2.imshow('front',front)
+        # cv2.imshow('back',back)
+        # cv2.waitKey(3000)
+        #
+        #
+        flag_front_flip=np.random.randint(0,2)
+        angle_front=np.random.randint(0,6)
+        if flag_front_flip==0:
+            front = rotate_bound(front, angle_front)
+            mask_front = cv2.threshold(front, 5, 1, type=cv2.THRESH_BINARY)[1]
+            mask_front_label = mask_front * 1
+        else:
+            front=cv2.flip(front,-1)
+            front=rotate_bound(front,angle_front)
+            mask_front=cv2.threshold(front,5,1,type=cv2.THRESH_BINARY)[1]
+            mask_front_label=mask_front*2
 
 
-flag_front_flip=np.random.randint(0,2)
-angle_front=np.random.randint(0,6)
-if flag_front_flip==0:
-    front = rotate_bound(front, angle_front)
-    mask_front = cv2.threshold(front, 5, 1, type=cv2.THRESH_BINARY)[1]
-    mask_front_label = mask_front * 1
-else:
-    front=cv2.flip(front,-1)
-    front=rotate_bound(front,angle_front)
-    mask_front=cv2.threshold(front,5,1,type=cv2.THRESH_BINARY)[1]
-    mask_front_label=mask_front*2
+        angle_back=np.random.randint(0,6)
+        flag_back_flip=np.random.randint(0,2)
+        if flag_back_flip==0:
+            back = rotate_bound(back, angle_back)
+            mask_back = cv2.threshold(back, 2, 255, type=cv2.THRESH_BINARY)[1]
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+            binary=mask_back[:,:,0]
+            binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
+            mask_back=np.uint8(np.stack([binary,binary,binary],axis=2)/255)
+
+            mask_back_label = mask_back * 3
+        else:
+            back=cv2.flip(back,-1)
+            back=rotate_bound(back,angle_back)
+            mask_back=cv2.threshold(back,2,255,type=cv2.THRESH_BINARY)[1]
+            binary = mask_back[:, :, 0]
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+            binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)  # 闭操作
+            mask_back = np.uint8(np.stack([binary, binary, binary], axis=2)/255)
+            mask_back_label = mask_back * 4
+
+        # cv2.imshow('hks',back)
+        # cv2.imshow('mask',mask_back*60)
+        # cv2.waitKey(0)
+        mask=np.zeros(shape=(image.shape[0],image.shape[1]),dtype=np.uint8)
+
+        h,w,c=front.shape
+        mask_roi_font=mask[150:(150+h),250:(250+w)]
+        mask_roi_font=mask_roi_font+mask_front_label[:,:,0]
+        mask[150:(150+h),250:(250+w)]=mask_roi_font
 
 
-angle_back=np.random.randint(0,6)
-flag_back_flip=np.random.randint(0,2)
-if flag_back_flip==0:
-    back = rotate_bound(back, angle_back)
-    mask_back = cv2.threshold(back, 2, 255, type=cv2.THRESH_BINARY)[1]
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    binary=mask_back[:,:,0]
-    binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
-    mask_back=np.uint8(np.stack([binary,binary,binary],axis=2)/255)
-
-    mask_back_label = mask_back * 3
-else:
-    back=cv2.flip(back,-1)
-    back=rotate_bound(back,angle_back)
-    mask_back=cv2.threshold(back,2,255,type=cv2.THRESH_BINARY)[1]
-    binary = mask_back[:, :, 0]
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)  # 闭操作
-    mask_back = np.uint8(np.stack([binary, binary, binary], axis=2)/255)
-    mask_back_label = mask_back * 4
-
-# cv2.imshow('hks',back)
-# cv2.imshow('mask',mask_back*60)
-# cv2.waitKey(0)
-mask=np.zeros(shape=(image.shape[0],image.shape[1]),dtype=np.uint8)
-
-h,w,c=front.shape
-mask_roi_font=mask[150:(150+h),250:(250+w)]
-mask_roi_font=mask_roi_font+mask_front_label[:,:,0]
-mask[150:(150+h),250:(250+w)]=mask_roi_font
-
-
-image_roi_font=image[150:(150+h),250:(250+w),:]
-image_roi_font=image_roi_font*(1-mask_front)
-image_with_logo_font=cv2.addWeighted(image_roi_font,1,front,1,0)
-image[150:(150+h),250:(250+w),:]=image_with_logo_font
+        image_roi_font=image[150:(150+h),250:(250+w),:]
+        image_roi_font=image_roi_font*(1-mask_front)
+        image_with_logo_font=cv2.addWeighted(image_roi_font,1,front,1,0)
+        image[150:(150+h),250:(250+w),:]=image_with_logo_font
 
 
 
-h_back,w_back,c_back=back.shape
-mask_roi_back=mask[550:(550+h_back),200:(200+w_back)]
-mask_roi_back=mask_roi_back+mask_back_label[:,:,0]
-mask[550:(550+h_back),200:(200+w_back)]=mask_roi_back
+        h_back,w_back,c_back=back.shape
+        mask_roi_back=mask[550:(550+h_back),200:(200+w_back)]
+        mask_roi_back=mask_roi_back+mask_back_label[:,:,0]
+        mask[550:(550+h_back),200:(200+w_back)]=mask_roi_back
 
 
-image_roi_back=image[550:(550+h_back),200:(200+w_back),:]
+        image_roi_back=image[550:(550+h_back),200:(200+w_back),:]
 
-# kernel_back = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
-# binary_mask=mask_back
-# binary_mask = cv2.morphologyEx(binary_mask, cv2.MORPH_ERODE, kernel_back)
+        # kernel_back = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
+        # binary_mask=mask_back
+        # binary_mask = cv2.morphologyEx(binary_mask, cv2.MORPH_ERODE, kernel_back)
 
-image_roi_back=image_roi_back*(1-mask_back)
-image_with_logo_back=cv2.addWeighted(image_roi_back,1,back,1,0)
-image[550:(550+h_back),200:(200+w_back),:]=image_with_logo_back
-# cv2.imwrite('/home/simple/mydemo/ocr_project/idcard_generator_project/idcard_generator/blend.jpg',image)
-# back=rotate_bound(back,5)
-# mask_back=cv2.threshold(back,5,255,type=cv2.THRESH_BINARY)
-tt=(1-mask_front)
-cv2.imshow('image',image)
-cv2.imshow('hks',back)
-cv2.imshow('mask',mask*100)
-cv2.waitKey(0)
+        image_roi_back=image_roi_back*(1-mask_back)
+        image_with_logo_back=cv2.addWeighted(image_roi_back,1,back,1,0)
+        image[550:(550+h_back),200:(200+w_back),:]=image_with_logo_back
+        # cv2.imwrite('/home/simple/mydemo/ocr_project/idcard_generator_project/idcard_generator/blend.jpg',image)
+        # back=rotate_bound(back,5)
+        # mask_back=cv2.threshold(back,5,255,type=cv2.THRESH_BINARY)
+        tt=(1-mask_front)
+        cv2.imshow('image',image)
+        cv2.imshow('hks',back)
+        cv2.imshow('mask',mask*100)
+        cv2.waitKey(0)
 
 
 
